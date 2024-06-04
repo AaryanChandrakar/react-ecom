24f0a71502fb7fe27a23ec55782f22d3931ba7d7
AaryanChandrakar <146125867+AaryanChandrakar@users.noreply.github.com>
2024-06-04 18:59:59 +0000

    Final Push

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -13,7 +13,7 @@ const Home = () => {
           />
           <div className="card-img-overlay d-flex align-items-center">
             <div className="container">
-              <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
+              <h5 className="card-title fs-1 text fw-lighter"></h5>
               <p className="card-text fs-5 d-none d-sm-block ">
               </p>
             </div>
